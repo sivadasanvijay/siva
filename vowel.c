3378ca62a1ab64b3173cb3d21947a9fbcb3f61bb
sivadasanvijay <sivadasanragu@gmail.com>
2016-09-17 11:57:47 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char w;
+printf("enter the word");
+scanf("%c",&w);
+if(w=='a'//w=='A'//w=='e'//w='E'//w='e'//w='i'//w='I'//w='o'//w='O'//w='u'//w='U')
+printf('the character is vowel");
+else
+printf("the character is not a vowel");
+}
+
