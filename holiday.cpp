cc03c341643897ef8438ba06815a483d49b1c3e4
sivadasanvijay <sivadasanragu@gmail.com>
2016-10-03 18:54:46 +0530

    Add files via upload

---
+++ holiday.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include(string.h>
+void main()
+{
+arr[]={"monday","tuesaday","wednessday","thursday","friday","saturday"}
+string day;
+cout<<"enter the day:";
+getline(cin,day);
+for(i=0;i<6;i++)
+{
+if(day==arr[i])
+cout<<"working day";}
+else
+cout<<"holiday";
+}
