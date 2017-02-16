9c165555d971e4abcd1f2aaeda34128a5051da92
sivadasanvijay <sivadasanragu@gmail.com>
2017-02-16 23:06:19 +0530

    Add files via upload

---
+++ New Text Document.java
@@ -0,0 +1,11 @@
+public class stringtoin
+{
+public static void main(string[] args)
+{
+Scanner s=new Scanner(System.in);
+String p=new String();
+p=s.next();
+int n=Integer.parseInt(p);
+System.out.println("String to integer conversion:"+n);
+}
+}
